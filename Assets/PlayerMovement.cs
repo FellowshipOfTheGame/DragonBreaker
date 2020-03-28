3ef8fbf3e9b5e141f95321134f9bfc165ff1a619
Luís Chaves <pi.luis.f3@usp.br>
2020-03-28 14:20:50 -0300

    Cooldown e quantidade arbitraria de dashes implementado. dashes ainda podem ser feitas para baixo quando grounded, entretanto

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController2D controller;      // Reference to a controller2D
+    [SerializeField] float runSpeed = 40f;                  // Speed of running
+    [SerializeField] float dashTime = 0.1f;                 // Total duration of a dash
+    bool jump = false;                                      // Makes the player jump when setted
+    float horizontalMove = 0f;                              // Direction of running * runSpeed
+    /*
+	 * Dash direction.
+	 * 0 - not dashing
+	 * 1 - right&up
+	 * 2 - right&down
+	 * 3 - left&up
+	 * 4 - left&down
+	 * 5 - right
+	 * 6 - left
+	 * 7 - up
+	 * 8 - down
+	 */
+    int dashDirection = 0;
+
+    void Update()
+    {
+        // Get controls
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool jumpDown = Input.GetButtonDown("Jump");
+        bool dashDown = Input.GetButtonDown("Dash");
+
+        // If jump is pressed, jumps;
+        if (jumpDown) jump = true;
+
+        // If arrows are pressed, sets the direction of move;
+        horizontalMove = horizontal * runSpeed;
+
+        // If dash is pressed
+        if (dashDown && dashDirection == 0)
+        {
+            // Determines a direction
+            if (horizontal > 0 && vertical > 0) dashDirection = 1;      //right&up
+            else if (horizontal > 0 && vertical < 0) dashDirection = 2; //right&down
+            else if (horizontal < 0 && vertical > 0) dashDirection = 3; //left&up
+            else if (horizontal < 0 && vertical < 0) dashDirection = 4; //left&down
+            else if (horizontal > 0) dashDirection = 5;                 //right
+            else if (horizontal < 0) dashDirection = 6;                 //left
+            else if (vertical > 0) dashDirection = 7;                   //up
+            else if (vertical < 0) dashDirection = 8;                   //down
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Moves the player
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+
+        // Dashes the player
+        if (dashDirection != 0)
+        {
+            controller.Dash(dashTime, dashDirection);
+            dashDirection = 0;
+        }
+    }
+}
--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -4,14 +4,18 @@ using UnityEngine.Events;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 825f;                            // Amount of force added when the player jumps.
-    [SerializeField] private float dashSpeed;                                   // Velocity when the player dashes.
-    [SerializeField] private float dashTime;                                  // Duration of the player dash *IN MILLISECONDS*.
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
 
-    private float dashTimeLeft;         // What time is left for the end of the dash
+    [SerializeField] private int qtdDash;                                       // Velocity when the player dashes.
+    [SerializeField] private float dashSpeed;                                   // Velocity when the player dashes.
+    [SerializeField] private float dashCooldown;                                // Velocity when the player dashes.
+    [SerializeField] private bool restoreDashOnLand = true;                     // Should dashes be restored when landed?
+
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;  // How much to smooth out the movement
+    [SerializeField] private bool airControl = false;                         // Whether or not a player can steer while jumping;
+
+    [SerializeField] private LayerMask whatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform groundCheck;                           // A position marking where to check if the player is grounded.
+
     /*
 	 * Dash direction.
 	 * 0 - not dashing
@@ -24,104 +28,83 @@ public class CharacterController2D : MonoBehaviour
 	 * 7 - up
 	 * 8 - down
 	 */
-    private int dashDirection = 0;
-    float horizontalMove = 0f;
-    private bool jump = false;
-    private float runSpeed = 40f;
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    private bool m_Grounded;            // Whether or not the player is grounded.
-    private Rigidbody2D m_Rigidbody2D;
-    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-    private Vector3 m_Velocity = Vector3.zero;
+    int dashDirection = 0;
+    float dashTimeLeft;                 // How much time is left for the end of the dash
+    int remainingDashes;                // Holds how much dashes the character can do 
 
-    Vector3 lineStart = Vector3.zero;
-    Vector3 lineEnd = Vector3.zero;
+    const float groundedRadius = .2f;   // Radius of the overlap circle to determine if grounded
+    bool grounded;                      // Whether or not the player is grounded.
+
+    Rigidbody2D rigidbody2D;
+    bool facingRight = true;            // For determining which way the player is currently facing.
+    Vector3 velocity = Vector3.zero;
 
     [Header("Events")]
     [Space]
 
     public UnityEvent OnLandEvent;
-    public UnityEvent OnDashEvent;
+    public UnityEvent OnDashStartEvent;
 
     private void Awake()
     {
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
 
-        // dashTime /= 1000;
+        remainingDashes = qtdDash;
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
-        if (OnDashEvent == null)
-            OnDashEvent = new UnityEvent();
-    }
-
-    private void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        bool jumpDown = Input.GetButtonDown("Jump");
-        bool dashDown = Input.GetButtonDown("Dash");
-
-        horizontalMove = horizontal * runSpeed;
-
-        // If jump is pressed;
-        if (jumpDown) jump = true;
-
-        // If dash is pressed
-        if (dashDown && dashDirection == 0)
-        {
-            dashTimeLeft = dashTime;
-            m_Rigidbody2D.velocity = Vector2.zero;
-            m_Rigidbody2D.angularVelocity = 0;
-
-            if (horizontal > 0 && vertical > 0) dashDirection = 1;      //right&up
-            else if (horizontal > 0 && vertical < 0) dashDirection = 2; //right&down
-            else if (horizontal < 0 && vertical > 0) dashDirection = 3; //left&up
-            else if (horizontal < 0 && vertical < 0) dashDirection = 4; //left&down
-            else if (horizontal > 0) dashDirection = 5;                 //right
-            else if (horizontal < 0) dashDirection = 6;                 //left
-            else if (vertical > 0) dashDirection = 7;                   //up
-            else if (vertical < 0) dashDirection = 8;                   //down
-
-            OnDashEvent.Invoke();
-        }
+        if (OnDashStartEvent == null)
+            OnDashStartEvent = new UnityEvent();
     }
 
     private void FixedUpdate()
     {
-        // Basic Movement
-        Move(horizontalMove * Time.fixedDeltaTime, jump);
-        jump = false;
-
-        // Dash Movement
+        // While there is time for dashing
+        dashTimeLeft -= Time.fixedDeltaTime;
+        // If dashTimeLeft is negativa and less than dashCoolDown, restores one dash and resets it
+        if (remainingDashes < qtdDash && Mathf.Abs(dashTimeLeft) > dashCooldown)
+        {
+            dashTimeLeft += dashCooldown;
+            remainingDashes++;
+        }
+        // If the player is dashing
         if (dashDirection != 0)
         {
+            // Dashes
+            if (dashDirection == 1) rigidbody2D.velocity = (Vector2.right + Vector2.up) * dashSpeed * Mathf.Sqrt(2) / 2;
+            else if (dashDirection == 2) rigidbody2D.velocity = (Vector2.right + Vector2.down) * dashSpeed * Mathf.Sqrt(2) / 2;
+            else if (dashDirection == 3) rigidbody2D.velocity = (Vector2.left + Vector2.up) * dashSpeed * Mathf.Sqrt(2) / 2;
+            else if (dashDirection == 4) rigidbody2D.velocity = (Vector2.left + Vector2.down) * dashSpeed * Mathf.Sqrt(2) / 2;
+            else if (dashDirection == 5) rigidbody2D.velocity = Vector2.right * dashSpeed;
+            else if (dashDirection == 6) rigidbody2D.velocity = Vector2.left * dashSpeed;
+            else if (dashDirection == 7) rigidbody2D.velocity = Vector2.up * dashSpeed;
+            else if (dashDirection == 8) rigidbody2D.velocity = Vector2.down * dashSpeed;
+
+            // Stops dashing
             if (dashTimeLeft <= 0)
             {
+                rigidbody2D.velocity = Vector2.zero;
                 dashDirection = 0;
-                m_Rigidbody2D.velocity = Vector2.zero;
-            } else
-            {
-                // While there is time for dashing
-                dashTimeLeft -= Time.fixedDeltaTime;
-                // Determine the velocity according to the direction
-                Dash(dashDirection);
             }
         }
 
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
+        bool wasGrounded = grounded;
+        grounded = false;
 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
             {
-                m_Grounded = true;
+                grounded = true;
                 if (!wasGrounded)
+                {
+                    // If it is supposed to restore dashes when landed;
+                    if (restoreDashOnLand) remainingDashes = qtdDash;
                     OnLandEvent.Invoke();
+                }
             }
         }
     }
@@ -129,50 +112,58 @@ public class CharacterController2D : MonoBehaviour
     public void Move(float move, bool jump)
     {
         //only control the player if grounded or airControl is turned on
-        if (dashDirection == 0 && m_Grounded || m_AirControl)
+        if (dashDirection == 0 && (grounded || airControl))
         {
             // Move the character by finding the target velocity
-            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+            rigidbody2D.velocity = Vector3.SmoothDamp(rigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
 
             // If the input is moving the player right and the player is facing left...
-            if (move > 0 && !m_FacingRight)
+            if (move > 0 && !facingRight)
             {
                 // ... flip the player.
                 Flip();
             }
             // Otherwise if the input is moving the player left and the player is facing right...
-            else if (move < 0 && m_FacingRight)
+            else if (move < 0 && facingRight)
             {
                 // ... flip the player.
                 Flip();
             }
         }
         // If the player should jump...
-        if (m_Grounded && jump)
+        if (grounded && jump)
         {
             // Add a vertical force to the player.
-            m_Grounded = false;
-            m_Rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+            grounded = false;
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
         }
     }
 
-    public void Dash(int direction)
+    public void Dash(float time, int direction)
     {
-        if (direction == 1) m_Rigidbody2D.velocity = (Vector2.right + Vector2.up) * dashSpeed;
-        else if (direction == 2) m_Rigidbody2D.velocity = (Vector2.right + Vector2.down) * dashSpeed;
-        else if (direction == 3) m_Rigidbody2D.velocity = (Vector2.left + Vector2.up) * dashSpeed;
-        else if (direction == 4) m_Rigidbody2D.velocity = (Vector2.left + Vector2.down) * dashSpeed;
-        else if (direction == 5) m_Rigidbody2D.velocity = Vector2.right * dashSpeed;
-        else if (direction == 6) m_Rigidbody2D.velocity = Vector2.left * dashSpeed;
-        else if (direction == 7) m_Rigidbody2D.velocity = Vector2.up * dashSpeed;
-        else if (direction == 8 ) m_Rigidbody2D.velocity = Vector2.down * dashSpeed;
+        // If the player is dashing or do not has remaining dashes, does not dash
+        if (dashDirection != 0 || remainingDashes == 0) return;
+
+        // Removes a dash
+        remainingDashes--;
+
+        // Stops the player
+        rigidbody2D.velocity = Vector2.zero;
+        rigidbody2D.angularVelocity = 0;
+
+        // Sets timeLeft and direction to the passed arguments
+        dashTimeLeft = time;
+        dashDirection = direction;
+
+        // Invokes a dashStart
+        OnDashStartEvent.Invoke();
     }
     private void Flip()
     {
         // Switch the way the player is labelled as facing.
-        m_FacingRight = !m_FacingRight;
+        facingRight = !facingRight;
 
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
