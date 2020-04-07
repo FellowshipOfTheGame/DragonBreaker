8da62c234a5c711483d206a561537294dae2f761
Luís Chaves <pi.luis.f3@usp.br>
2020-04-07 00:12:05 -0300

    Refactor: Movement improvement

--- Assets/Player/CharacterController2D.cs
+++ Assets/Player/CharacterController2D.cs
@@ -6,6 +6,7 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float jumpForce = 825f;                            // Amount of force added when the player jumps.
 
     [SerializeField] private int qtdDash = 1;                                       // Velocity when the player dashes.
+    [SerializeField] private float dashTime = 0.1f;                                   // Duration of a dash.
     [SerializeField] private float dashSpeed = 25;                                   // Velocity when the player dashes.
 
     [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;  // How much to smooth out the movement
@@ -14,19 +15,8 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private LayerMask whatIsGround;                          // A mask determining what is ground to the character
     [SerializeField] private Transform groundCheck;                           // A position marking where to check if the player is grounded.
 
-    /*
-	 * Dash direction.
-	 * 0 - not dashing
-	 * 1 - right&up
-	 * 2 - right&down
-	 * 3 - left&up
-	 * 4 - left&down
-	 * 5 - right
-	 * 6 - left
-	 * 7 - up
-	 * 8 - down
-	 */
-    int dashDirection = 0;
+    Vector2 dashDirection;
+    bool dash = false;
     float dashTimeLeft;                 // How much time is left for the end of the dash
     int remainingDashes;                // Holds how much dashes the character can do 
 
@@ -42,6 +32,7 @@ public class CharacterController2D : MonoBehaviour
 
     public UnityEvent OnLandEvent;
     public UnityEvent OnDashStartEvent;
+    public UnityEvent OnDashEndEvent;
 
     private void Awake()
     {
@@ -53,30 +44,26 @@ public class CharacterController2D : MonoBehaviour
             OnLandEvent = new UnityEvent();
         if (OnDashStartEvent == null)
             OnDashStartEvent = new UnityEvent();
+        if (OnDashEndEvent == null)
+            OnDashEndEvent = new UnityEvent();
     }
 
     private void FixedUpdate()
     {
         // If the player is dashing
-        if (dashDirection != 0)
+        if (dash)
         {
-        // While there is time for dashing
-        dashTimeLeft -= Time.fixedDeltaTime;
-            // Dashes
-            if (dashDirection == 1) rigidbody2D.velocity = (Vector2.right + Vector2.up) * dashSpeed * Mathf.Sqrt(2) / 2;
-            else if (dashDirection == 2) rigidbody2D.velocity = (Vector2.right + Vector2.down) * dashSpeed * Mathf.Sqrt(2) / 2;
-            else if (dashDirection == 3) rigidbody2D.velocity = (Vector2.left + Vector2.up) * dashSpeed * Mathf.Sqrt(2) / 2;
-            else if (dashDirection == 4) rigidbody2D.velocity = (Vector2.left + Vector2.down) * dashSpeed * Mathf.Sqrt(2) / 2;
-            else if (dashDirection == 5) rigidbody2D.velocity = Vector2.right * dashSpeed;
-            else if (dashDirection == 6) rigidbody2D.velocity = Vector2.left * dashSpeed;
-            else if (dashDirection == 7) rigidbody2D.velocity = Vector2.up * dashSpeed;
-            else if (dashDirection == 8) rigidbody2D.velocity = Vector2.down * dashSpeed;
+            // While there is time for dashing, dashes
+            dashTimeLeft -= Time.fixedDeltaTime;
+            rigidbody2D.velocity = dashDirection * dashSpeed;
 
             // Stops dashing
             if (dashTimeLeft <= 0)
             {
                 rigidbody2D.velocity = Vector2.zero;
-                dashDirection = 0;
+                // Invokes a dashEnd
+                OnDashEndEvent.Invoke();
+                dash = false;
             }
         }
 
@@ -104,7 +91,7 @@ public class CharacterController2D : MonoBehaviour
     public void Move(float move)
     {
         //only control the player if grounded or airControl is turned on
-        if (dashDirection == 0 && (grounded || airControl))
+        if (grounded || airControl)
         {
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
@@ -137,10 +124,10 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-    public void Dash(float time, int direction)
+    public void Dash(Vector2 direction)
     {
         // If the player is dashing or do not has remaining dashes, does not dash
-        if (dashDirection != 0 || remainingDashes == 0) return;
+        if (remainingDashes == 0) return;
 
         // Removes a dash
         remainingDashes--;
@@ -149,8 +136,9 @@ public class CharacterController2D : MonoBehaviour
         rigidbody2D.velocity = Vector2.zero;
         rigidbody2D.angularVelocity = 0;
 
-        // Sets timeLeft and direction to the passed arguments
-        dashTimeLeft = time;
+        // Starts dash
+        dash = true;
+        dashTimeLeft = dashTime;
         dashDirection = direction;
 
         // Invokes a dashStart
