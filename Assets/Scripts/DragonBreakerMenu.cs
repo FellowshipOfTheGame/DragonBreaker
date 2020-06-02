31136acbae3bf3931dadb1b955f9cf9292f0e375
Tiago M. Silva <tmsilva11@gmail.com>
2020-06-01 23:55:58 -0300

    Add: Main Menu base, no animations
    Added abstract Menu State Manager, main menu base  logic and background music.

---
+++ Assets/Scripts/DragonBreakerMenu.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class DragonBreakerMenu : MenuStateManager
+{
+    [SerializeField] protected InputActionAsset ActionsAsset;
+    [SerializeField] protected MenuState[] MenuStates;
+
+    protected void Awake()
+    {
+        //Populate possible states dictionary
+        PopulateMenuDictionary();
+        //Start on first State
+        ChangeState(MenuStates[0]);
+        ActionsAsset["Submit"].canceled += TransitionToMain;
+        ActionsAsset["Click"].canceled += TransitionToMain;
+        ActionsAsset["Cancel"].canceled += TransitionBackToPressPlay;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadCharacterSelectionScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void TransitionBackToPressPlay(InputAction.CallbackContext obj)
+    {
+        //If state is Main Menu, go back to Press Play to Begin
+        if(_actualState.StateName == MenuStates[1].StateName)
+        {
+            ChangeState(MenuStates[0]); 
+        }
+        //Else if state is Configurations, go back to Main Menu
+        else if (_actualState.StateName == MenuStates[3].StateName)
+        {
+            ChangeState(MenuStates[1]);
+        }
+        //Else if state is Credits, go back to Main Menu
+        else if (_actualState.StateName == MenuStates[4].StateName)
+        { 
+            ChangeState(MenuStates[1]);
+        }
+    }
+
+    protected void PopulateMenuDictionary()
+    {
+        foreach(var state in MenuStates)
+        {
+            states.Add(state.StateName, state);
+        }
+    }
+
+    private void TransitionToMain(InputAction.CallbackContext obj)
+    {
+        if(_actualState.StateName == MenuStates[0].StateName)
+        {
+            //Change State
+            ChangeState(MenuStates[1]);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ActionsAsset["Submit"].canceled -= TransitionToMain;
+        ActionsAsset["Click"].canceled -= TransitionToMain;
+        ActionsAsset["Cancel"].canceled -= TransitionBackToPressPlay;
+    }
+
+
+
+}
---
+++ Assets/Scripts/MenuStateManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public abstract class MenuStateManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct MenuState {
+        public string StateName;
+        public UnityEvent OnEnterState;
+        public UnityEvent OnExitState;
+    };
+
+    protected Dictionary<string, MenuState> states = new Dictionary<string, MenuState>();
+    protected MenuState _actualState;
+
+    public virtual void ChangeState(string stateName)
+    {
+        //Check if state name is valid
+        if (states.ContainsKey(stateName))
+        {
+            //Invoke Exit State Event
+            _actualState.OnExitState?.Invoke();
+            Debug.Log($"Exited: {_actualState.StateName}");
+            //Change actual state
+            _actualState = states[stateName];
+            //Invoke Enter State Event
+            _actualState.OnEnterState?.Invoke();
+            Debug.Log($"Entered: {_actualState.StateName}");
+        }
+        else
+        {
+            //Send warning message
+            Debug.LogWarning($"Trying to access unexisting menu state: {stateName}.");
+        }
+    }
+
+    public virtual void ChangeState(MenuState state)
+    {
+        //Check if state name is valid
+        if (states.ContainsValue(state))
+        {
+            //Invoke Exit State Event
+            _actualState.OnExitState?.Invoke();
+            Debug.Log($"Exited: {_actualState.StateName}");
+            //Change actual state
+            _actualState = state;
+            //Invoke Enter State Event
+            _actualState.OnEnterState?.Invoke();
+            Debug.Log($"Entered: {_actualState.StateName}");
+        }
+        else
+        {
+            //Send warning message
+            Debug.LogWarning($"Trying to access unexisting menu state: {state.StateName}.");
+        }
+    }
+
+}
