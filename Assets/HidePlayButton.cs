870a1bb90eaf67a8e9ee29decca2d4b5a20c29c8
Tiago M. Silva <tmsilva11@gmail.com>
2020-07-22 20:29:54 -0300

    Build Adjustments

---
+++ Assets/HidePlayButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class HidePlayButton : MonoBehaviour
+{
+    [SerializeField] private Image _PlayButton;
+    [SerializeField] private int minimumToDisplay = 2;
+
+    private PlayerInputManager _manager;
+
+    private void Awake()
+    {
+        _manager = GetComponent<PlayerInputManager>();
+    }
+
+
+    public void DisplayCheck()
+    {
+        Debug.Log(_manager.playerCount);
+        if(_manager.playerCount < minimumToDisplay)
+        {
+            _PlayButton.enabled = false;
+        }
+        else
+        {
+            _PlayButton.enabled = true;
+        }
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput) => DisplayCheck();
+    public void OnPlayerLeft(PlayerInput playerInput) => DisplayCheck();
+}
