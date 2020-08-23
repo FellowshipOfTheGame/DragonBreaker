95543c0a9d44e975e93ab631b5be60a5b8842b34
Renato Francói Amprino <renato.francoi.amprino@gmail.com>
2020-08-23 01:47:24 -0300

    Countdown sounds and animation tweaks
    > A different sound can be played when called (used in the BREAK count)

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -8,20 +8,24 @@ using UnityEngine.UI;
 public class Countdown : MonoBehaviour
 {
     public AudioSource backgroundMusic;
-    private AudioSource countdownSound;
+    public AudioSource countdownSound;
+    public AudioSource finalCountdownSound;
 
     public bool InCountdown;
-
 
     public void StartCountdown()
     {
         MultiplayerManager.Instance.DeactivatePlayersInputs();      //input disabled during countdown
-        countdownSound = gameObject.GetComponent<AudioSource>();
         InCountdown = true;
     }
     public void Count()    //play countdown sound
     {
         countdownSound.Play();
+    }
+
+    public void FinalCount()    //play the last countdown sound (BREAK)
+    {
+        finalCountdownSound.Play();
     }
 
     public void EndCountdown()  //input and music re-enabled as countdown ends
